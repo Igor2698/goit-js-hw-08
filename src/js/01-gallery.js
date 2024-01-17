55a35c557a21c89bb75f39ac96d4597ec35d69e5
Igor2698 <128493043+Igor2698@users.noreply.github.com>
2024-01-17 11:40:10 +0200

    +

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector('.gallery')
+const markup = galleryItems.map(({ preview, original, description }) => `
+    <li class ='gallery__item'>
+        <a href="${original}" class="gallery__item">
+            <img src="${preview}" alt="${description}" data-source="${original}" class="gallery__image">
+        </a>
+    </li>`).join('');
+list.innerHTML = markup;
+list.addEventListener('click', onListClick);
 
-console.log(galleryItems);
+function onListClick(ev) {
+    ev.preventDefault();
+    if (ev.target.nodeName !== 'IMG') {
+        return
+    }
+    console.log(ev.target.dataset.source);
+    const instance = basicLightbox.create(` < div class= "modal" >
+    <img src="${ev.target.dataset.source}">
+       </div>`, { closable: false, })
+    instance.show();
+
+
+    function onKeyDown(event) {
+        if (event.key === 'Escape') {
+            instance.close();
+            document.removeEventListener('keydown', onKeyDown);
+        }
+    }
+
+    document.addEventListener('keydown', onKeyDown);
+}
+
+
